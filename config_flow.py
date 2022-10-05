bb4f1e4f3413bdaa6967094f64de4878d599bd49
Toni Naukkarinen <toni.naukkarinen@gef.fi>
2022-10-05 10:02:34 +0300

    Fix reauth implementation.

--- config_flow.py
+++ config_flow.py
@@ -35,41 +35,15 @@ GEF_VISION_DATA_SCHEMA = {
         default=DEFAULT_SCAN_INTERVAL,
     ): int,
 }
+
+GEF_VISION_REAUTH_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_PASSWORD, description="Password"): str
+    }
+)
 GEF_VISION_USER_SCHEMA = vol.Schema(GEF_VISION_DATA_SCHEMA)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(GEF_VISION_DATA_SCHEMA)
-
-
-class CannotConnect(HomeAssistantError):
-    """Error to indicate we cannot connect."""
-
-
-class InvalidAuth(HomeAssistantError):
-    """Error to indicate there is invalid auth."""
-
-
-class InvalidParameter(HomeAssistantError):
-    """Error to indicate an invalid parameter."""
-
-
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
-    """Attempts to connect to GEF Vision using the provided credentials"""
-    client = Client(VISION_BASE_URL)
-    auth = AuthToken(
-        username=data[CONF_USERNAME], password=data[CONF_PASSWORD], token=""
-    )
-
-    if data[CONF_SCAN_INTERVAL] < 10:
-        raise InvalidParameter
-
-    try:
-        token = await api_v2_auth_token_create.asyncio(client=client, json_body=auth)
-        if not token:
-            raise InvalidAuth
-    except (ConnectError, ConnectTimeout) as exc:
-        raise CannotConnect from exc
-
-    return data[CONF_USERNAME].lower()
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -77,54 +51,80 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
+    def __init__(self) -> None:
+        self._reauth = False
+        self._username = None
+        self._password = None
+        self._scan_interval = 0
+
+    async def _async_show_error_form(self, errors: dict, error_step: str, schema: vol.Schema) -> FlowResult:
+        return self.async_show_form(
+            step_id=error_step,
+            data_schema=schema,
+            errors=errors
+        )
+
+    async def _async_validate_config(self) -> FlowResult:
+        """Attempts to connect to GEF Vision using the provided credentials"""
+        client = Client(VISION_BASE_URL)
+        auth = AuthToken(
+            username=self._username, password=self._password, token=""
+        )
+
+        if self._scan_interval < 10:
+            return await self._async_show_error_form({'base': 'invalid_parameter'}, 'user', GEF_VISION_USER_SCHEMA)
+
+        try:
+            token = await api_v2_auth_token_create.asyncio(client=client, json_body=auth)
+            if not token:
+                return await self._async_show_error_form({'base': 'invalid_auth'}, 'user', GEF_VISION_USER_SCHEMA)
+        except (ConnectError, ConnectTimeout) as exc:
+            return await self._async_show_error_form({'base': 'cannot_connect'}, 'user', GEF_VISION_USER_SCHEMA)
+
+        if self._reauth:
+            entry = await self.async_set_unique_id(self._username.lower())
+            if entry:
+                self.hass.config_entries.async_update_entry(
+                    entry,
+                    data={**entry.data, CONF_PASSWORD: self._password}
+                )
+                self.hass.async_create_task(
+                    self.hass.config_entries.async_reload(entry.entry_id)
+                )
+                return self.async_abort(reason="reauth_successful")
+        return self.async_create_entry(title=self._username.lower(), data={CONF_USERNAME: self._username, CONF_PASSWORD: self._password, CONF_SCAN_INTERVAL: self._scan_interval})
+
     async def async_step_user(self, info: dict[str, Any]) -> FlowResult:
-        errors = {}
         if info is not None:
-            errors = {}
             await self.async_set_unique_id(info[CONF_USERNAME].lower())
             self._abort_if_unique_id_configured()
-            try:
-                title = await validate_input(self.hass, info)
-                return self.async_create_entry(title=title, data=info)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-                raise PlatformNotReady("Connection to GEF Vision failed")
-            except InvalidAuth:
-                errors["base"] = "invalid_auth"
-                raise ConfigEntryAuthFailed(
-                    "Invalid authentication data for GEF Vision"
-                )
-            except InvalidParameter:
-                errors["base"] = "invalid_parameter"
-                raise PlatformNotReady("Invalid parameter was provided")
+
+            self._reauth = False
+            self._scan_interval = info[CONF_SCAN_INTERVAL]
+            self._username = info[CONF_USERNAME]
+            self._password = info[CONF_PASSWORD]
+            return await self._async_validate_config()
+
         return self.async_show_form(
-            step_id="user", data_schema=GEF_VISION_USER_SCHEMA, errors=errors
+            step_id="user", data_schema=GEF_VISION_USER_SCHEMA
         )
 
     async def async_step_reauth(self, entry_data: Mapping[str, Any]) -> FlowResult:
         """Perform reauth upon an API authentication error."""
-        self._reauth_config_entry = self.hass.config_entries.async_get_entry(
-            self.context["entry_id"]
-        )
+        self._reauth = True
+        self._username = entry_data[CONF_USERNAME]
+        self._scan_interval = entry_data[CONF_SCAN_INTERVAL]
         return await self.async_step_reauth_confirm()
 
     async def async_step_reauth_confirm(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Confirm reauth dialog."""
-        errors = {}
         if not user_input:
             return self.async_show_form(
-                step_id="user", data_schema=GEF_VISION_USER_SCHEMA, errors=errors
+                step_id="reauth_confirm",
+                data_schema=GEF_VISION_REAUTH_SCHEMA
             )
-        try:
-            title = await validate_input(self.hass, user_input)
-        except CannotConnect:
-            errors["base"] = "cannot_connect"
-            raise PlatformNotReady("Connection to GEF Vision failed")
-        except InvalidAuth:
-            errors["base"] = "invalid_auth"
-            raise ConfigEntryAuthFailed("Invalid authentication data for GEF Vision")
-        except InvalidParameter:
-            errors["base"] = "invalid_parameter"
-            raise PlatformNotReady("Invalid parameter was provided")
+        self._password = user_input[CONF_PASSWORD]
+        return await self._async_validate_config()
+
